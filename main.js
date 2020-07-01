408aa07941bd6784758fa6a5e35265b183740dde
Miguel <miguel@admins-Mac-mini.local>
2020-07-01 10:34:36 -0300

    DoIt v.1.0 UI started; Some window properties have been changed.

--- main.js
+++ main.js
@@ -2,14 +2,29 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
 	let win = new BrowserWindow({
-		width: 1000,
-		height: 650,
+		show: false,
+		title: "DoIt",
+		width: 420,
+		maxWidth: 420,
+		minWidth: 330,
+		height: 750,
+		maxHeight: 750,
+		minHeight: 380,
+		maximizable: false,
+		fullscreenable: false,
+		// alwaysOnTop: true,
+		hasShadow: false,
+		titleBarStyle: "hidden",
 		webPreferences: {
+			devTools: false,
 			nodeIntegration: true,
 		},
 	});
 
-	win.loadFile("index.html");
+	win.loadFile("src/index.html");
+	win.once("ready-to-show", () => {
+		win.show();
+	});
 }
 
 app.whenReady().then(createWindow);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+function navigate() {
+	window.location.href = "teste.html";
+}
+
+function teste() {
+	alert("teste deu certo");
+}
