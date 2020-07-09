88f2ae82470512cae6bcef27da76b9baf5431229
Miguel <miguel@admins-Mac-mini.local>
2020-07-09 14:35:18 -0300

    DoIt v.1.0 Login and Register ages and functionalities done; Authentication error handling created; Loading screen created.

--- src/Main.js
+++ src/Main.js
@@ -17,48 +17,128 @@ firebase.initializeApp({
 function isLogedIn() {
 	firebase.auth().onAuthStateChanged((user) => {
 		if (user) {
-			alert(`Email: ${user.email}`);
+			window.location.href = "./Main.html";
 		} else {
-			alert("Faça o login primeiro");
+			document.getElementById("loaderContainer").style.display = "none";
+			document.getElementById("interactionArea").style.display = "flex";
 		}
 	});
 }
+function Logout() {
+	firebase
+		.auth()
+		.signOut()
+		.then(() => {
+			window.location.href = "./Initial.html";
+		});
+}
 
 function Login() {
-	let email = document.getElementById("loginEmail").value;
-	let password = document.getElementById("loginPassword").value;
+	let emailInput = document.getElementById("loginEmail");
+	let passwordInput = document.getElementById("loginPassword");
 
-	if (email.length > 0 && password.length > 0) {
+	if (emailInput.value.length > 0 && passwordInput.value.length >= 0) {
 		firebase
 			.auth()
-			.signInWithEmailAndPassword(email, password)
-			.then(() => alert("Login realizado"))
+			.signInWithEmailAndPassword(emailInput.value, passwordInput.value)
+			.then(() => {
+				window.location.href = "./Main.html";
+			})
 			.catch((error) => {
+				if (error.code === "auth/user-not-found") {
+					return alert("Este usuário não existe, cadastre-se primeiro!");
+				}
+				if (error.code === "auth/invalid-email") {
+					return (
+						alert("Este email é inválido, tente outro!"), emailInput.focus()
+					);
+				}
+				if (error.code === "auth/wrong-password") {
+					return (
+						alert("A senha ou email está incorreto, por favor tente novamente"),
+						passwordInput.focus()
+					);
+				}
+				if (error.code === "auth/network-request-failed") {
+					return alert("Sem conexão à rede, conecte-se e tente novamente");
+				}
+				if (error.code === "auth/too-many-requests") {
+					return alert(
+						"Você fez muitas tentativas de login sem sucesso, por favor, tente novamente mais tarde!"
+					);
+				}
 				console.log(error);
-				alert("Algo deu errado, por favor, tente novamente!");
+				alert(
+					"Houve um problema, por favor reinicie o aplicativo e tente novamente!"
+				);
 			});
 	} else {
 		alert("Digite seu email e senha antes de prosseguir");
 	}
 }
 
-function Register() {
-	let email = document.getElementById("registerEmail").value;
-	let password = document.getElementById("registerPassword").value;
-
-	if (email.length > 0 && password.length > 0) {
+function forgotPassword() {
+	let email = document.getElementById("loginEmail").value;
+	if (email.length > 0) {
 		firebase
 			.auth()
-			.createUserWithEmailAndPassword(email, password)
+			.sendPasswordResetEmail(email)
+			.then(() =>
+				alert("Um email para recuperação de senha foi enviado para você!")
+			);
+	} else {
+		alert(
+			"Digite o seu email no campo de 'email' para que possamos enviar o email de recuperação!"
+		);
+	}
+}
+
+function Register() {
+	let emailInput = document.getElementById("registerEmail");
+	let passwordInput = document.getElementById("registerPassword");
+
+	if (emailInput.value.length > 0 && passwordInput.value.length >= 6) {
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(emailInput.value, passwordInput.value)
 			.then(() => {
 				alert("Usuário criado com sucesso");
+				window.location.href = "./Main.html";
 			})
 			.catch((error) => {
+				if (error.code === "auth/email-already-in-use") {
+					return (
+						alert(
+							"Este email já está sendo usado por algum usuário, por favor use outro!"
+						),
+						emailInput.focus()
+					);
+				}
+				if (error.code === "auth/invalid-email") {
+					return (
+						alert("Este endereç de email é inválido, tente outro!"),
+						emailInput.focus()
+					);
+				}
+				if (error.code === "auth/network-request-failed") {
+					return alert("Sem conexão à rede, conecte-se e tente novamente");
+				}
+				if (error.code === "auth/weak-password") {
+					return (
+						alert(
+							"Essa senha está fraca, ela deve ter pelo menos 6 caracteres!"
+						),
+						passwordInput.focus()
+					);
+				}
 				console.log(error);
 				alert("Houve um erro no seu cadastro, por favor tente novamente!");
 			});
 	} else {
-		alert("Complete os campos para prosseguir!");
+		alert(
+			"Complete os campos para prosseguir, a senha deve ter pelo menos 6 caracteres!"
+		);
+		passwordInput.focus();
 	}
 }
 
