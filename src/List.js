f972474667d77f1c85073cffd552d47f4e581cb7
Miguel <miguel@admins-Mac-mini.local>
2020-07-13 11:27:02 -0300

    DoIt v.1.0 Items rendering in real time; Icons are not rendering properly.

--- src/List.js
+++ src/List.js
@@ -2,6 +2,10 @@ const firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
 
+const list = document.getElementById("list");
+const dataInput = document.getElementById("input");
+const addButton = document.getElementById("addItemButton");
+
 firebase.initializeApp({
 	apiKey: "AIzaSyBiSc-DqKtwdz7HZgY7pPUKhWl98e9Ee-w",
 	authDomain: "to-do-app-d4a0c.firebaseapp.com",
@@ -26,6 +30,7 @@ function loadData() {
 			.onSnapshot((data) => {
 				let retrievedList = data.data().list;
 				console.log(retrievedList);
+				renderTodos(retrievedList);
 			});
 	});
 }
@@ -37,4 +42,42 @@ function Logout() {
 		.then(() => {
 			window.location.href = "./Initial.html";
 		});
+}
+
+function renderTodos(data) {
+	list.innerHTML = "";
+	data.forEach((item) => {
+		// Criar li class='listItem'
+		let listItem = document.createElement("li");
+		listItem.setAttribute("class", "listItem");
+
+		// criar div com icone da estrela class='starIcon' icone=> class='fas fa-star'
+		let starDiv = document.createElement("div");
+		starDiv.setAttribute("class", "starIcon");
+		let star = document.createElement("i");
+		star.setAttribute("class", "fa fas-star");
+		// let star = document.createTextNode("\u2605");
+		starDiv.appendChild(star);
+
+		// criar div com texto class='textContainer'
+		let textDiv = document.createElement("div");
+		textDiv.setAttribute("class", "textContainer");
+		let itemText = document.createTextNode(item.title);
+		textDiv.appendChild(itemText);
+
+		// criar div com ione do lixo class='trashIcon' icone=> class='fas fa-trash-alt'
+		let trashDiv = document.createElement("div");
+		trashDiv.setAttribute("class", "trashIcon");
+		let trash = document.createElement("i");
+		trash.setAttribute("class", "fa fas-trash-alt");
+		trashDiv.appendChild(trash);
+
+		//Junta todas as partes do item da lista
+		listItem.appendChild(starDiv);
+		listItem.appendChild(textDiv);
+		listItem.appendChild(trashDiv);
+
+		//Coloca item na lista em si
+		list.appendChild(listItem);
+	});
 }
